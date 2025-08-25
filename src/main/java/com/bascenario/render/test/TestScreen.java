c2b8e90fc1b3fc594fd112e2335a8ca3c43b2e7a
oryxel1 <gamingnoob681@gmail.com>
2025-08-25 10:40:59 +0700

    Cleanup the test screen.

--- src/main/java/com/bascenario/render/test/TestScreen.java
+++ src/main/java/com/bascenario/render/test/TestScreen.java
@@ -10,7 +10,12 @@ import imgui.ImVec2;
 import java.awt.*;
 
 public class TestScreen extends Screen {
+    private final Screen screen;
     public boolean appear;
+
+    public TestScreen() {
+        this.screen = this;
+    }
 
     @Override
     public void init() {
@@ -37,23 +42,19 @@ public class TestScreen extends Screen {
         });
 
         ImVec2 size2 = ImGui.getFont().calcTextSizeA(13, Float.MAX_VALUE, 0, "Click me too!");
-        this.components.add(new ClickableComponent(5, 300, (int) size2.x, (int) size2.y, () -> {
-            Launcher.WINDOW.setCurrentScreen(new Screen() {
-                @Override
-                public void init() {
-                    ImVec2 size1 = ImGui.getFont().calcTextSizeA(13, Float.MAX_VALUE, 0, "Click me to go back!");
-                    this.components.add(new ClickableComponent(5, 100, (int) size1.x, (int) size1.y, () -> {
-                        Launcher.WINDOW.setCurrentScreen(new TestScreen());
-                    }) {
-                        @Override
-                        public void render(int width, int height, double mouseX, double mouseY) {
-                            super.render(width, height, mouseX, mouseY);
-                            ImGui.getForegroundDrawList().addText(new ImVec2(x, y), Color.BLACK.getRGB(), "Click me to go back!");
-                        }
-                    });
-                }
-            });
-        }) {
+        this.components.add(new ClickableComponent(5, 300, (int) size2.x, (int) size2.y, () -> Launcher.WINDOW.setCurrentScreen(new Screen() {
+            @Override
+            public void init() {
+                ImVec2 size1 = ImGui.getFont().calcTextSizeA(13, Float.MAX_VALUE, 0, "Click me to go back!");
+                this.components.add(new ClickableComponent(5, 100, (int) size1.x, (int) size1.y, () -> Launcher.WINDOW.setCurrentScreen(screen)) {
+                    @Override
+                    public void render(int width, int height, double mouseX, double mouseY) {
+                        super.render(width, height, mouseX, mouseY);
+                        ImGui.getForegroundDrawList().addText(new ImVec2(x, y), Color.BLACK.getRGB(), "Click me to go back!");
+                    }
+                });
+            }
+        })) {
             @Override
             public void render(int width, int height, double mouseX, double mouseY) {
                 super.render(width, height, mouseX, mouseY);
@@ -68,5 +69,9 @@ public class TestScreen extends Screen {
         if (appear) {
             ImGui.getForegroundDrawList().addText(new ImVec2(50, 50), Color.BLACK.getRGB(), "Hi there, im new here!");
         }
+
+        ImGui.getForegroundDrawList().addCircle(new ImVec2(50, 400), 50, Color.BLACK.getRGB());
+        ImGui.getForegroundDrawList().addCircle(new ImVec2(150, 400), 50, Color.BLACK.getRGB());
+        ImGui.getForegroundDrawList().addCircle(new ImVec2(250, 400), 50, Color.BLACK.getRGB());
     }
 }
