ccfe8dfd1d3742706394fb53f08ea654fbf76e01
oryxel1 <gamingnoob681@gmail.com>
2025-10-01 13:42:02 +0700

    Some cool changes to the title screen.

--- src/main/java/com/bascenario/render/main/TitleScreen.java
+++ src/main/java/com/bascenario/render/main/TitleScreen.java
@@ -13,6 +13,7 @@ import com.bascenario.util.render.FontUtil;
 import com.bascenario.util.render.RenderUtil;
 import com.esotericsoftware.spine.*;
 import com.esotericsoftware.spine.utils.TwoColorPolygonBatch;
+import net.lenni0451.commons.RandomUtils;
 import net.raphimc.thingl.ThinGL;
 import net.raphimc.thingl.implementation.window.WindowInterface;
 import net.raphimc.thingl.resource.image.texture.Texture2D;
@@ -33,6 +34,9 @@ public class TitleScreen extends Screen {
     private TextureAtlas atlas;
 
     private int cachedSoundId;
+
+    private long lastTime = System.currentTimeMillis(), waitDuration;
+    private int animationState = 0;
     public void init() {
         this.initComponents();
 
@@ -55,9 +59,15 @@ public class TitleScreen extends Screen {
         this.skeleton = new Skeleton(skeletonData);
 
         this.skeleton.setPosition(0, 0);
-        this.state = new AnimationState(new AnimationStateData(skeletonData));
+        final AnimationStateData stateData = new AnimationStateData(skeletonData);
+        stateData.setMix("Pat_01_A", "Dev_Eye", 0.5F);
+        stateData.setMix("Dev_Eye", "Pat_01_M", 0.5F);
+        stateData.setMix("Pat_01_M", "Pat_01_A", 0.5F);
+
+        this.state = new AnimationState(stateData);
 
         this.state.addAnimation(0, "Idle_01", true, 0);
+        this.state.addAnimation(1, "Pat_01_A", true, 0);
         this.init = true;
     }
 
@@ -70,6 +80,27 @@ public class TitleScreen extends Screen {
 
     @Override
     public void render(Matrix4fStack positionMatrix, WindowInterface window, double mouseX, double mouseY) {
+        if (this.waitDuration == 0) {
+            this.waitDuration = RandomUtils.randomLong(8000L, 16000L);
+//            System.out.println("Wait: " + this.waitDuration);
+        }
+
+        if (System.currentTimeMillis() - this.lastTime > this.waitDuration) {
+            animationState++;
+            if (animationState > 2) {
+                this.animationState = 0;
+            }
+
+            switch (animationState) {
+                case 0 -> this.state.addAnimation(1, "Pat_01_A", true, 0);
+                case 1 -> this.state.addAnimation(1, "Dev_Eye", true, 0);
+                case 2 -> this.state.addAnimation(1, "Pat_01_M", true, 0);
+            }
+
+            this.lastTime = System.currentTimeMillis();
+            this.waitDuration = RandomUtils.randomLong(8000L, 16000L);
+        }
+
         // Render hina home sprite :D (background)!
         final float delta = Gdx.graphics.getDeltaTime();
         this.state.update(delta);
