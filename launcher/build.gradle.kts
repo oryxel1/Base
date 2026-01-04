373d819905b59915da4bd17ed89ec4975f27440e
oryxel1 <gamingnoob681@gmail.com>
2026-01-04 19:16:31 +0700

    Added Github Workflows.

--- launcher/build.gradle.kts
+++ launcher/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version("8.3.0")
 }
 
 dependencies {
     implementation(project(":api"))
     implementation(project(":editor"))
+    implementation(project(":serializer"))
     implementation(project(":core"))
 
     implementation(libs.gdx.base)
@@ -20,4 +22,15 @@ dependencies {
     implementation(libs.raphimc.audiomixer)
 
     implementation(libs.commons.animations)
+}
+
+tasks {
+    shadowJar {
+        archiveFileName = "Base.jar"
+        manifest {
+            attributes(
+                "Main-Class" to "oxy.bascenario.Main"
+            )
+        }
+    }
 }
