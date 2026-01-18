27a3a72dee9dd2030fc7f373c04a52667cc66b01
oryxel1 <gamingnoob681@gmail.com>
2026-01-18 11:14:26 +0700

    Initial work.

--- core/src/main/java/oxy/bascenario/managers/AnimationManager.java
+++ core/src/main/java/oxy/bascenario/managers/AnimationManager.java
@@ -52,59 +52,59 @@ public class AnimationManager extends HashMap<String, Animation> implements Anim
     }
 
     private void initDefaultAnimations() {
-        this.put(
-                "bascenarioengine:default-shake",
-                Animation.builder()
-                        .name("Default Shake")
-                        .offset(new AnimationValue(new String[] {"math.abs(query.offset(0) + 19.2) <= 0.0001 ? 19.2 : -19.2", "0"}, "0.08", Easing.LINEAR))
-                        .defaultOffset(new AnimationValue(new String[]{"0", "0"}, "0.08", Easing.LINEAR))
-                        .maxDuration(0.26f)
-                        .resetWhenFinish(true)
-                        .build()
-        );
-
-        this.put(
-                "bascenarioengine:down-then-up",
-                Animation.builder()
-                        .name("Down Then Up")
-                        .put(0, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"0", "108"}, "0.3", Easing.LINEAR)).build())
-                        .defaultOffset(new AnimationValue(new String[]{"0", "0"}, "0.3", Easing.LINEAR))
-                        .maxDuration(0.3f)
-                        .resetWhenFinish(true)
-                        .build()
-        );
-
-        this.put(
-                "bascenarioengine:hangry",
-                Animation.builder()
-                        .name("Default Angry")
-                        .put(0, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"0", "-108"}, "0.2", Easing.LINEAR)).build())
-                        .put(0.2f, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"0", "0"}, "0.2", Easing.LINEAR)).build())
-                        .put(0.4f, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"0", "-108"}, "0.2", Easing.LINEAR)).build())
-                        .defaultOffset(new AnimationValue(new String[]{"0", "0"}, "0.3", Easing.LINEAR))
-                        .maxDuration(0.6f)
-                        .resetWhenFinish(true)
-                        .build()
-        );
-
-        this.put(
-                "bascenarioengine:test",
-                Animation.builder()
-                        .name("Dev Test")
-                        .put(0, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"-960", "0"}, "1", Easing.LINEAR)).build())
-                        .put(1f, AnimationTimeline.builder().scale(new AnimationValue(new String[] {"1.5", "1.5"}, "0.3", Easing.QUAD)).build())
-                        .put(1.3f, AnimationTimeline.builder().scale(new AnimationValue(new String[] {"1", "1"}, "0.3", Easing.QUAD)).build())
-                        .defaultOffset(new AnimationValue(new String[]{"0", "0"}, "0.1", Easing.LINEAR))
-                        .maxDuration(1.6f)
-                        .resetWhenFinish(true)
-                        .build()
-        );
-
-        this.put(
-                "bascenarioengine:loop-test",
-                Animation.builder()
-                        .name("Dev Loop Test")
-                        .rotation(new AnimationValue(new String[] {"0", "0", "57.29577951308232 * (math.mod(q.currentTimeMillis, 500) / 500 * math.pi * 2)"}, "0", Easing.LINEAR)).build()
-        );
+//        this.put(
+//                "bascenarioengine:default-shake",
+//                Animation.builder()
+//                        .name("Default Shake")
+//                        .offset(new AnimationValue(new String[] {"math.abs(query.offset(0) + 19.2) <= 0.0001 ? 19.2 : -19.2", "0"}, "0.08", Easing.LINEAR))
+//                        .defaultOffset(new AnimationValue(new String[]{"0", "0"}, "0.08", Easing.LINEAR))
+//                        .maxDuration(0.26f)
+//                        .resetWhenFinish(true)
+//                        .build()
+//        );
+//
+//        this.put(
+//                "bascenarioengine:down-then-up",
+//                Animation.builder()
+//                        .name("Down Then Up")
+//                        .put(0, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"0", "108"}, "0.3", Easing.LINEAR)).build())
+//                        .defaultOffset(new AnimationValue(new String[]{"0", "0"}, "0.3", Easing.LINEAR))
+//                        .maxDuration(0.3f)
+//                        .resetWhenFinish(true)
+//                        .build()
+//        );
+//
+//        this.put(
+//                "bascenarioengine:hangry",
+//                Animation.builder()
+//                        .name("Default Angry")
+//                        .put(0, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"0", "-108"}, "0.2", Easing.LINEAR)).build())
+//                        .put(0.2f, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"0", "0"}, "0.2", Easing.LINEAR)).build())
+//                        .put(0.4f, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"0", "-108"}, "0.2", Easing.LINEAR)).build())
+//                        .defaultOffset(new AnimationValue(new String[]{"0", "0"}, "0.3", Easing.LINEAR))
+//                        .maxDuration(0.6f)
+//                        .resetWhenFinish(true)
+//                        .build()
+//        );
+//
+//        this.put(
+//                "bascenarioengine:test",
+//                Animation.builder()
+//                        .name("Dev Test")
+//                        .put(0, AnimationTimeline.builder().offset(new AnimationValue(new String[] {"-960", "0"}, "1", Easing.LINEAR)).build())
+//                        .put(1f, AnimationTimeline.builder().scale(new AnimationValue(new String[] {"1.5", "1.5"}, "0.3", Easing.QUAD)).build())
+//                        .put(1.3f, AnimationTimeline.builder().scale(new AnimationValue(new String[] {"1", "1"}, "0.3", Easing.QUAD)).build())
+//                        .defaultOffset(new AnimationValue(new String[]{"0", "0"}, "0.1", Easing.LINEAR))
+//                        .maxDuration(1.6f)
+//                        .resetWhenFinish(true)
+//                        .build()
+//        );
+//
+//        this.put(
+//                "bascenarioengine:loop-test",
+//                Animation.builder()
+//                        .name("Dev Loop Test")
+//                        .rotation(new AnimationValue(new String[] {"0", "0", "57.29577951308232 * (math.mod(q.currentTimeMillis, 500) / 500 * math.pi * 2)"}, "0", Easing.LINEAR)).build()
+//        );
     }
 }
