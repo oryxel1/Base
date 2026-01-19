12291d4d757b5c75dcfbe52be5fd1049217ea90e
oryxel1 <gamingnoob681@gmail.com>
2026-01-19 18:11:56 +0700

    Make sound file info serializing nullable.

--- serializer/src/main/java/oxy/bascenario/serializers/types/effects/SoundType.java
+++ serializer/src/main/java/oxy/bascenario/serializers/types/effects/SoundType.java
@@ -12,7 +12,7 @@ public class SoundType implements Type<Sound> {
     public JsonElement write(Sound sound) {
         final JsonObject object = new JsonObject();
         object.addProperty("id", sound.id());
-        object.add("file", Types.FILE_INFO_TYPE.write(sound.file()));
+        object.add("file", Types.NULLABLE_FILE_INFO_TYPE.write(sound.file()));
         object.addProperty("max-volume", sound.maxVolume());
         object.addProperty("loop", sound.loop()); // tbh this is never used in the editor itself buuuuuuuuut.
         return object;
@@ -21,19 +21,19 @@ public class SoundType implements Type<Sound> {
     @Override
     public Sound read(JsonElement element) {
         final JsonObject object = element.getAsJsonObject();
-        return new Sound(object.get("id").getAsInt(), Types.FILE_INFO_TYPE.read(object.get("file")), object.get("max-volume").getAsFloat(), object.get("loop").getAsBoolean());
+        return new Sound(object.get("id").getAsInt(), Types.NULLABLE_FILE_INFO_TYPE.read(object.get("file")), object.get("max-volume").getAsFloat(), object.get("loop").getAsBoolean());
     }
 
     @Override
     public void write(Sound sound, ByteBuf buf) {
         buf.writeInt(sound.id());
-        Types.FILE_INFO_TYPE.write(sound.file(), buf);
+        Types.NULLABLE_FILE_INFO_TYPE.write(sound.file(), buf);
         buf.writeFloat(sound.maxVolume());
         buf.writeBoolean(sound.loop());
     }
 
     @Override
     public Sound read(ByteBuf buf) {
-        return new Sound(buf.readInt(), Types.FILE_INFO_TYPE.read(buf), buf.readFloat(), buf.readBoolean());
+        return new Sound(buf.readInt(), Types.NULLABLE_FILE_INFO_TYPE.read(buf), buf.readFloat(), buf.readBoolean());
     }
 }
