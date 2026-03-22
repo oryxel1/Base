9045e45d3fdfc746e3e4053dc8f86a1daf7c2604
oryxel1 <gamingnoob681@gmail.com>
2026-03-22 15:43:52 +0700

    Make this build.

--- serializer/src/main/java/oxy/bascenario/serializers/types/event/impl/SetWeatherType.java
+++ serializer/src/main/java/oxy/bascenario/serializers/types/event/impl/SetWeatherType.java
@@ -2,7 +2,6 @@ package oxy.bascenario.serializers.types.event.impl;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
-import io.netty.buffer.ByteBuf;
 import oxy.bascenario.api.effects.Weather;
 import oxy.bascenario.api.event.SetWeatherEvent;
 import oxy.bascenario.serializers.base.TypeWithName;
@@ -21,15 +20,5 @@ public class SetWeatherType implements TypeWithName<SetWeatherEvent> {
     @Override
     public SetWeatherEvent read(JsonElement element) {
         return new SetWeatherEvent(Weather.valueOf(element.getAsString()));
-    }
-
-    @Override
-    public void write(SetWeatherEvent setWeatherEvent, ByteBuf buf) {
-        buf.writeInt(setWeatherEvent.weather().ordinal());
-    }
-
-    @Override
-    public SetWeatherEvent read(ByteBuf buf) {
-        return new SetWeatherEvent(Weather.values()[buf.readInt()]);
     }
 }
