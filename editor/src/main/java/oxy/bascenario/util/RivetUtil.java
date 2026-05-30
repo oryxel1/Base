a40e374490bdac265d887fa9bab3eed2a8ccdda9
oryxel1 <gamingnoob681@gmail.com>
2026-05-30 23:31:38 +0700

    stuff.

--- editor/src/main/java/oxy/bascenario/util/RivetUtil.java
+++ editor/src/main/java/oxy/bascenario/util/RivetUtil.java
@@ -1,41 +1,31 @@
 package oxy.bascenario.util;
 
-import net.lenni0451.rivet.Rivet;
-import net.lenni0451.rivet.backend.Renderer;
-import net.lenni0451.rivet.component.Container;
-import net.lenni0451.rivet.component.base.Button;
-import net.lenni0451.rivet.component.base.ComboBox;
+import net.lenni0451.rivet.component.container.Button;
+import net.lenni0451.rivet.component.container.ComboBox;
+import net.lenni0451.rivet.component.container.Container;
 import net.lenni0451.rivet.component.impl.Label;
 import net.lenni0451.rivet.layout.anchor.AnchorLayoutOptions;
 import net.lenni0451.rivet.layout.list.VerticalListLayout;
-import net.lenni0451.rivet.math.Rectangle;
+import net.lenni0451.rivet.text.model.TextOrigin;
 import oxy.bascenario.utils.math.Pair;
 
 import java.util.List;
 
 public class RivetUtil {
-    public static void menuList(Rivet rivet, Container container, String name, AnchorLayoutOptions layout, List<Pair<String, Runnable>> lists) {
-        final Container buttons = new Container(rivet, new VerticalListLayout(5, true)) {
-            @Override
-            public void render(Renderer renderer, Rectangle bounds) {
-                renderer.scale(0.45f, () -> super.render(renderer, bounds));
-            }
-        };
+    public static void dropDownList(Container container, String name, AnchorLayoutOptions layout, List<Pair<String, Runnable>> lists) {
+        final Container buttons = new Container(new VerticalListLayout(1, true));
 
-        final ComboBox box = new ComboBox(rivet, name, buttons) {
-            @Override
-            public void render(Renderer renderer, Rectangle bounds) {
-                renderer.scale(0.45f, () -> super.render(renderer, bounds));
-            }
-        };
+        final ComboBox box = new ComboBox(new Label(name).horizontalOrigin(TextOrigin.Horizontal.LOGICAL_LEFT).scale(0.45f), buttons);
         box.arrowSize().set(0f);
         box.layoutOptions(layout);
 
-        for (Pair<String, Runnable> button : lists) {
-            buttons.addChild(new Button(rivet, new Label(rivet, button.left()), c -> {
+        for (Pair<String, Runnable> pair : lists) {
+            Button button = new Button(new Label(pair.left()).scale(0.45f), c -> {
                 box.close();
-                button.right().run();
-            }));
+                pair.right().run();
+            });
+            button.cornerRadius().set(0f);
+            buttons.addChild(button);
         }
 
         container.addChild(box);
